63088fcd0b188faa9eef7c41133b9830ad16e355
Jacob Taylor <ez5504@gmail.com>
2019-10-11 10:39:12 -0400

    adding front door unlock automation

--- Puppet.Automation/LockFrontDoor.cs
+++ Puppet.Automation/LockFrontDoor.cs
@@ -1,63 +1,49 @@
-﻿// using Puppet.Common.Automation;
-// using Puppet.Common.Devices;
-// using Puppet.Common.Events;
-// using Puppet.Common.Services;
-// using System;
-// using System.Collections.Generic;
-// using System.Text;
-// using System.Threading.Tasks;
+﻿using Puppet.Common.Automation;
+using Puppet.Common.Devices;
+using Puppet.Common.Events;
+using Puppet.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-// namespace Puppet.Automation
-// {
-//     [TriggerDevice("Contact.FrontDoor", Capability.Contact)]
-//     [TriggerDevice("Lock.FrontDoorDeadbolt", Capability.Lock)]
-//     public class LockFrontDoor : AutomationBase
-//     {
-//         ContactSensor _frontDoor;
-//         LockDevice _frontDoorLock;
-//         List<Speaker> _notificationDevices;
+namespace Puppet.Automation
+{
+    [TriggerDevice("Lock.FrontDoorDeadbolt", Capability.Lock)]
+    public class LockFrontDoor : AutomationBase
+    {
+        LockDevice _frontDoorLock;
 
-//         public LockFrontDoor(HomeAutomationPlatform hub, HubEvent evt) : base(hub, evt)
-//         {}
+        DimmerSwitchRelay _livingRoomLight;
 
-//         protected override async Task InitDevices()
-//         {
-//             _frontDoor =
-//                 await _hub.GetDeviceByMappedName<ContactSensor>("Contact.FrontDoor");
+        Weather _weather;
 
-//             _frontDoorLock =
-//                 await _hub.GetDeviceByMappedName<LockDevice>("Lock.FrontDoorDeadbolt");
+        public LockFrontDoor(HomeAutomationPlatform hub, HubEvent evt) : base(hub, evt)
+        {}
 
-//             _notificationDevices =
-//                 new List<Speaker>() {
-//                     await _hub.GetDeviceByMappedName<Speaker>("Speaker.WebhookNotifier"),
-//                     await _hub.GetDeviceByMappedName<Speaker>("Speaker.KitchenSpeaker")
-//                 };
-//         }
+        protected override async Task InitDevices()
+        {
+            _frontDoorLock =
+                await _hub.GetDeviceByMappedName<LockDevice>("Lock.FrontDoorDeadbolt");
 
-//         protected override async Task Handle()
-//         {
-//             if(_evt.Value.Contains("locking") && _evt.DeviceId == _frontDoorLock.Id)
-//             {
-//                 return;
-//             }
+            _livingRoomLight =
+                await _hub.GetDeviceByMappedName<DimmerSwitchRelay>("Switch.LivingRoom");
+            
+            _weather = await _hub.GetDeviceByMappedName<Weather>("Misc.Weather");
+        }
 
-//             await WaitForCancellationAsync(TimeSpan.FromMinutes(5));
-
-//             int attempts = 0;
-//             while (_frontDoor.Status == ContactStatus.Closed && 
-//                     _frontDoorLock.Status != LockStatus.Locked &&
-//                     attempts < 3)
-//             {
-//                 attempts++;
-//                 await _frontDoorLock.Lock();
-//                 await WaitForCancellationAsync(TimeSpan.FromSeconds(30));
-//             }
-
-//             if (_frontDoorLock.Status != LockStatus.Locked)
-//             {
-//                 await _notificationDevices.Speak("Front door deadbolt failed to lock! Please check it.");
-//             }
-//         }
-//     }
-// }
+        protected override async Task Handle()
+        {
+            if(_frontDoorLock.Status == LockStatus.Unlocked) 
+            {
+                DateTime sunsetTime = await _weather.SunsetTime();
+                var currTime = DateTime.Now;
+                if(currTime > (sunsetTime.AddDays(-1.5)))
+                {
+                    await _livingRoomLight.On();
+                    await _livingRoomLight.SetBrightness(100);
+                }
+            }
+        }
+    }
+}
